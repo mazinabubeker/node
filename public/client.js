988a1c003392bb97cbd9e178981dea968ac2d73d
Mazin Abubeker <mazin@Mazins-MacBook-Pro.local>
2020-07-29 02:51:52 -0700

    Piano

--- public/client.js
+++ public/client.js
@@ -1,27 +1,76 @@
 var socket;
-var ball_element;
 
-$(document).ready(function(){    
-    socket = io();
-    // socket.emit('mouse', data);
-    document.getElementById('name').addEventListener('keypress', e=>{
-        if(window.event.keyCode==13){e.preventDefault();}else{return;}
-        if(document.getElementById('name').value == ''){return;}
-        socket.emit('new_user', document.getElementById('name').value);
-    });
+let osc, playing, freq, amp;
+var keys = "QWERTYUIOP";
+var num_keys = 10;
+var key_ids = [];
 
-    socket.on('new_user_resp', name=>{
-        addNewUser(name, true);
-    });
-    socket.on('new_user_resp_2', name=>{
-        addNewUser(name, false);
-    });
-});
-
-function addNewUser(name, boolbool){
-    let new_element = `<div class='user'>` + name + `</div>`;
-    document.getElementById('box').insertAdjacentHTML('afterbegin', new_element);
-    if(!boolbool){
-        document.getElementById('name').remove();
+function setup(){
+    osc = []
+    playing = []
+    for(var i = 0; i < num_keys; i++){
+        osc.push(new p5.Oscillator('sine'));
+        osc[i].freq(2100*(i+1)/10);
+        osc[i].amp(0);
+        playing.push(false);
     }
 }
+
+// if(window.event.keyCode==13){e.preventDefault();}else{return;}
+$(document).ready(function(){    
+    socket = io();
+    document.querySelector('body').focus();
+    document.querySelector('body').addEventListener("keydown", playKey);
+    document.querySelector('body').addEventListener("keyup", stopKey);
+    for(var i = 0; i < keys.length; i++){
+        key_ids.push(keys[i].charCodeAt(0));
+    }
+
+    
+    let piano_container = document.getElementById('container');
+    for(var i = 0; i < keys.length; i++){
+        piano_container.insertAdjacentHTML('beforeend', `<div class='key'></div>`);
+    }
+    
+
+    // document.querySelector('html').keyPressed(startTone);
+    // document.querySelector('html').keyPressed(stopTone);
+
+    socket.on('play_key_resp', forcePlayAtIndex);
+    socket.on('stop_key_resp', forceStopAtIndex);
+});
+
+
+function playKey(e){
+    if(!key_ids.includes(window.event.keyCode)){return;}
+    let index = key_ids.indexOf(window.event.keyCode);
+    if(playing[index]){return;}
+    osc[index].start();
+    osc[index].amp(.3);
+    playing[index] = true;
+    socket.emit('play_key', index);
+    document.getElementsByClassName('key')[index].style.backgroundColor = "#42b6f5";
+}
+
+function forcePlayAtIndex(index){
+    osc[index].start();
+    osc[index].amp(.3);
+    playing[index] = true;
+    document.getElementsByClassName('key')[index].style.backgroundColor = "#f77272";
+}
+
+function stopKey(e){
+    if(!key_ids.includes(window.event.keyCode)){return;}
+    let index = key_ids.indexOf(window.event.keyCode);
+    osc[index].amp(0);
+    playing[index] = false;
+    socket.emit('stop_key', index);
+    document.getElementsByClassName('key')[index].style.backgroundColor = "white";
+}
+
+function forceStopAtIndex(index){
+    osc[index].amp(0);
+    playing[index] = false;
+    document.getElementsByClassName('key')[index].style.backgroundColor = "white";
+}
+
--- server.js
+++ server.js
@@ -6,15 +6,12 @@ app.use(express.static('public'));
 var io = soccc(server);
 io.sockets.on('connection', newConnection)
 
-var users = []
-
 function newConnection(socket){
   console.log("Connected: " + socket.id);
-  socket.on('new_user', username=>{
-    console.log(username);
-    users.push(username);
-    console.log("ho");
-    socket.broadcast.emit('new_user_resp', username);
-    socket.emit('new_user_resp_2', username);
+  socket.on('play_key', index=>{
+    socket.broadcast.emit('play_key_resp', index);
+  });
+  socket.on('stop_key', index=>{
+    socket.broadcast.emit('stop_key_resp', index);
   });
 }
