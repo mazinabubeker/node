f0bcd5f21bbb440d90e661efc641f966cdf9bced
Mazin Abubeker <mazin@Mazins-MacBook-Pro.local>
2020-07-28 22:52:22 -0700

    Updated server.js

--- server.js
+++ server.js
@@ -1,22 +1,8 @@
-
 var express = require('express');
 var soccc = require('socket.io');
-// var PORT = 3000;
 var app = express();
 var server = app.listen(process.env.PORT || 3000);
-// const server = app.listen(PORT, () => console.log(`Listening on ${ PORT }`));
 app.use(express.static('public'));
-// app.use(express.json({limit: '1mb'}));
-// app.post('/query_post', (req, res) => {
-//   res.send(req.body);
-//   res.end();
-// });  
-// app.get('/query_get', (req, res) => {
-//   res.send(nextFlashTime.toString());
-//   res.end();
-// });
-
-
 var io = soccc(server);
 io.sockets.on('connection', newConnection)
 
@@ -28,8 +14,3 @@ function newConnection(socket){
     socket.broadcast.emit('mouse', data);
   }
 }
-
-
-
-
-// app.get(url, onQuery(input, output));
