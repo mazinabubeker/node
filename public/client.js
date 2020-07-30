9f52311bea8aa4333e3186cebe59fbb03b22a215
Mazin Abubeker <mazin@Mazins-MacBook-Pro.local>
2020-07-29 23:52:06 -0700

    hey

--- public/client.js
+++ public/client.js
@@ -1,76 +1,97 @@
 var socket;
 
-function setData(x,y,z){
-    document.querySelector('body').innerHTML = "X: " + x + "<br>" + "Y: " + y + "<br>" + "Z: " + z;
-    
+var data = {x: 0, y: 0, z: 0, j: 0, k: 0, l: 0}
+var scene = new THREE.Scene();
+var camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+
+var renderer = new THREE.WebGLRenderer();
+renderer.setSize( window.innerWidth, window.innerHeight );
+
+var cube;
+
+function setData(){
+    document.body.innerHTML = `x: ` + data.x + `<br>y: ` + data.y + `<br>z: ` + data.z + `<br>j: ` + data.j + `<br>k: ` + data.k + `<br>l: ` + data.l;
 }
 
 $(document).ready(function(){    
-    // socket = io();
-    // if (window.DeviceOrientationEvent) {
-    //     setData(0,0,0);
-    //     window.addEventListener("deviceorientation", function (event) {
-    //         setData(event.alpha, event.beta, event.gamma);
-    //     });
-    // }
-
-    
-
-    // if (window.DeviceOrientationEvent) {
-    //     window.addEventListener('deviceorientation', (e)=>{
-    //       setData(event.alpha, event.beta, event.gamma);
-    //     });
-    //     console.log("fff");
-    //   }
-
-    //   if (window.DeviceMotionEvent) {
-    //     window.addEventListener('devicemotion', (e)=>{
-    //       // document.querySelector('body').innerHTML = "INDEED OH MY DO IT DO IT";
-    //     });
-    //     console.log("fff");
-    //     document.querySelector('body').style.backgroundColor = "green";
-    //   }
+    socket = io();
+    document.body.style.backgroundColor = "green";
+    socket.on('three_time', executeThree);
+    socket.on('update_rotation', info=>{
+      data.x = info.x;
+      data.y = info.y;
+      data.z = info.z;
+    });
 });
 
+function animate() {
+	requestAnimationFrame( animate );
+  renderer.render( scene, camera );
+  cube.rotation.x = data.x;
+  cube.rotation.y = data.y;
+  cube.rotation.z = data.z;
+}
+
+function executeThree(){
+  document.body.onclick = "";
+  document.body.appendChild( renderer.domElement );
+
+  var geometry = new THREE.BoxGeometry();
+  var material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
+  cube = new THREE.Mesh( geometry, material );
+  scene.add(cube);
+
+  camera.position.z = 5;
+  animate();
+}
+
 function startRequest(){
+  document.body.style.backgroundColor = "blue";
   if (typeof DeviceOrientationEvent.requestPermission === 'function') {
+    document.body.style.backgroundColor = "yellow";
     DeviceOrientationEvent.requestPermission()
       .then(permissionState => {
         if (permissionState === 'granted') {
+          // User accepted
           window.addEventListener('deviceorientation', () => {
-            setData(event.alpha, event.beta, event.gamma);
+            document.body.style.backgroundColor = "orange";
+
+            data.x = event.alpha;
+            data.y = event.beta;
+            data.z = event.gamma;
+            socket.io('new_rotation', {x: event.alpha, y: event.beta, z: event.gamma});
+            setData();
           });
         }
       })
       .catch(e=>{
-        document.querySelector('body').innerHTML = "Not granted";
+        // User declined
       });
   } else {
-    window.addEventListener('deviceorientation', () => {
-      setData(event.alpha, event.beta, event.gamma);
-    });
+    // Has access
   }
+
+  // if (typeof DeviceMotionEvent.requestPermission === 'function') {
+  //   DeviceMotionEvent.requestPermission()
+  //     .then(permissionState => {
+  //       if (permissionState === 'granted') {
+  //         // User accepted
+  //         window.addEventListener('devicemotion', () => {
+  //             data.j = event.acceleration.x;
+  //             data.k = event.acceleration.y;
+  //             data.l = event.acceleration.z;
+  //             // setData();
+  //         });
+  //       }
+  //     })
+  //     .catch(e=>{
+  //       // User declined
+  //     });
+  // } else {
+  //   // User already has access
+
+  // }
 }
-
-// import React from 'react';
-// import ReactDOM from 'react-dom';
-
-// var nah = 5;
-
-// ReactDOM.render(
-//     <App />,
-//     document.getElementById('root')
-// );
-
-// class App extends React.Component{
-//     render(){
-//         return (
-//             <div id="status">{nah}</div>
-//         );
-//     }
-// }
-
-
 
 
 
--- server.js
+++ server.js
@@ -6,8 +6,19 @@ app.use(express.static('public'));
 var io = soccc(server);
 io.sockets.on('connection', newConnection)
 
+var count = 0;
+
 function newConnection(socket){
   console.log("Connected: " + socket.id);
+  count++;
+  if(count == 1){
+    socket.emit('three_time', 0);
+  }else{
+    socket.on('new_rotation', data=>{
+      socket.broadcast.emit('update_rotation', data);
+    });
+  }
+  
 }
 
 
