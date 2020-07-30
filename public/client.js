5bff400d356028e3aa30255e980f43a1e0d37b11
Mazin Abubeker <mazin@Mazins-MacBook-Pro.local>
2020-07-30 12:04:21 -0700

    Yes

--- public/client.js
+++ public/client.js
@@ -1,6 +1,6 @@
 var socket;
 
-var data = {x: 0, y: 0, z: 0, j: 0, k: 0, l: 0}
+var data = {x: 1, y: 1, z: 1, j: 0, k: 0, l: 0}
 var scene = new THREE.Scene();
 var camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
 var light;
@@ -39,20 +39,41 @@ function executeListener(){
   cube = new THREE.Mesh( geometry, material );
   scene.add(cube);
 
-  light = new THREE.PointLight(0xFFFFFF,1,500);
-  light.position.set(10,0,25);
+  light = new THREE.PointLight(0xFFFFFF,1.1,500);
+  light.position.set(0,0,25);
   scene.add(light);
 
   camera.position.z = 5;
+  camera.rotation.order = 'ZXY';
   animate();
 }
 
 function animate() {
 	requestAnimationFrame( animate );
   renderer.render( scene, camera );
-  cube.rotation.x = degrees_to_radians(data.x);
-  cube.rotation.y = degrees_to_radians(data.y);
-  cube.rotation.z = degrees_to_radians(data.z);
+  // cube.rotation.x = degrees_to_radians(data.y);
+  // cube.rotation.y = degrees_to_radians(data.z);
+  // cube.rotation.z = degrees_to_radians(data.x);
+  cube.rotation.x += .02;
+  cube.rotation.y += .02;
+  // if(data.x/20 < 1){
+  //   cube.scale.x = 1;
+  // }else{
+  //   cube.scale.x = data.x/20;
+  // }
+  // if(data.y/6 < 1){
+  //   cube.scale.y = 1;
+  // }else{
+  //   cube.scale.y = data.y/6;
+  // }
+  // if(data.z/6 < 1){
+  //   cube.scale.z = 1;
+  // }else{
+  //   cube.scale.z = data.z/6;
+  // }
+  cube.scale.x = 1 + data.x/100;
+  cube.scale.y = 1 + data.x/100;
+  cube.scale.z = 1 + data.x/100;
 }
 
 function executeSender(){
@@ -72,6 +93,7 @@ function startRequest(){
             // if(data.x == Math.round(event.alpha) || data.y == Math.round(event.beta) || data.z == Math.round(event.gamma)){
             //   return;
             // }
+            if(Math.abs(event.alpha - data.x) > 30){return;}
             data.x = event.alpha;
             data.y = event.beta+180;
             data.z = event.gamma+180;
