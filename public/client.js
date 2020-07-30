061c7e9a31e4a75b55cd9995ed88955cb42c81fb
Mazin Abubeker <mazin@Mazins-MacBook-Pro.local>
2020-07-30 10:59:34 -0700

    Added white bgnd

--- public/client.js
+++ public/client.js
@@ -5,6 +5,7 @@ var scene = new THREE.Scene();
 var camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
 var light;
 var renderer = new THREE.WebGLRenderer({antialias: true});
+renderer.setClearColor("#ffffff");
 renderer.setSize( window.innerWidth, window.innerHeight );
 
 var cube;
