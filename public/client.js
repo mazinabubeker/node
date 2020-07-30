80e2478ed04f84e40f1f26b6df58f31074051ba1
Mazin Abubeker <mazin@Mazins-MacBook-Pro.local>
2020-07-29 17:44:57 -0700

    Gyro

--- public/client.js
+++ public/client.js
@@ -1,38 +1,40 @@
 var socket;
-var gyroData = {a: 0, x: 0, y: 0, z: 0, q: 0, r: 0}
+var gyroData = {x: 0, y: 0, z: 0, a: 0, j: 0, k: 0}
 $(document).ready(function(){    
     socket = io();
-    if (window.DeviceOrientationEvent) {
-        window.addEventListener("deviceorientation", function (event) {
-            gyroData.a = event.beta;
-            gyroData.x = event.gamma;
-            gyroData.y = event.alpha;
-            gyroData.z = event.absolute;
-            setData();
-        }, true);
-    }
-    if (window.DeviceMotionEvent) {
-        window.addEventListener('devicemotion', function (event) {
-            console.log("BEGER")
-            gyroData.y = event.acceleration.x;
-            gyroData.z = event.acceleration.y;
-            setData();
-        }, true);
-    }
-    setData();
+
+    // if (window.DeviceOrientationEvent) {
+    //     setData();
+    //     window.addEventListener("deviceorientation", function (event) {
+    //         gyroData.x = event.alpha;
+    //         gyroData.y = event.beta;
+    //         gyroData.z = event.gamma;
+    //         gyroData.a = event.absolute;
+    //     }, true);
+    // }
+    // if (window.DeviceMotionEvent) {
+    //     setData();
+    //     window.addEventListener('devicemotion', function (event) {
+    //         gyroData.j = event.acceleration.x;
+    //         gyroData.k = event.acceleration.y;
+    //     }, true);
+    // }
+    let gyroscope = new Gyroscope({frequency: 60});
+
+    gyroscope.addEventListener('reading', e => {
+        setData(gyroscope.x, gyroscope.y, gyroscope.z)
+    });
+    gyroscope.start();
 });
 
 
 
-function setup(){
-    // window.addEventListener('deviceorientation', rotatePhone, true);
+// function setData(){
+//     document.querySelector('body').innerHTML = "X: " + gyroData.x + "<br>" + "Y: " + gyroData.y + "<br>" + "Z: " + gyroData.z + "<br>" + "A: " + gyroData.a + "<br>" + "J: " + gyroData.j + "<br>" + "K: " + gyroData.k;
+// }
+
+function setData(x,y,z){
+    document.querySelector('body').innerHTML = "X: " + x + " " + y + " " + z;
 }
 
-function setData(){
-    document.querySelector('body').innerHTML = "A: " + gyroData.a + "<br>" + "X: " + gyroData.x + "<br>" + "Y: " + gyroData.y + "<br>" + "Z: " + gyroData.z + "<br>" + "Q: " + gyroData.q + "<br>" + "R: " + gyroData.r;
-}
-
-function draw(){
-
-}
 
