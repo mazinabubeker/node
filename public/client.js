187f3913286dabde3f629247d2d70b9b95b76a1e
Mazin Abubeker <mazin@Mazins-MacBook-Pro.local>
2020-07-30 17:12:33 -0700

    Hi

--- public/client.js
+++ public/client.js
@@ -54,23 +54,6 @@ function animate() {
   cube.rotation.x = degrees_to_radians(data.y);
   cube.rotation.y = degrees_to_radians(data.z);
   cube.rotation.z = degrees_to_radians(data.x);
-  // cube.rotation.x += .02;
-  // cube.rotation.y += .02;
-  // if(data.x/20 < 1){
-  //   cube.scale.x = 1;
-  // }else{
-  //   cube.scale.x = data.x/20;
-  // }
-  // if(data.y/6 < 1){
-  //   cube.scale.y = 1;
-  // }else{
-  //   cube.scale.y = data.y/6;
-  // }
-  // if(data.z/6 < 1){
-  //   cube.scale.z = 1;
-  // }else{
-  //   cube.scale.z = data.z/6;
-  // }
   // cube.scale.x = 1 + data.x/100;
   // cube.scale.y = 1 + data.x/100;
   // cube.scale.z = 1 + data.x/100;
@@ -108,7 +91,18 @@ function startRequest(){
       });
     } else {
       // Has access
-      console.log("Has access");
+      window.addEventListener('deviceorientation', () => {
+        // document.body.style.backgroundColor = "black";
+        // if(data.x == Math.round(event.alpha) || data.y == Math.round(event.beta) || data.z == Math.round(event.gamma)){
+        //   return;
+        // }
+        if(Math.abs(event.alpha - data.x) > 30){return;}
+        data.x = event.alpha;
+        data.y = event.beta+180;
+        data.z = event.gamma+180;
+        setData();
+        socket.emit('new_rotation', {x: event.alpha, y: event.beta+180, z: event.gamma+180});
+      });
     }
 }
 
